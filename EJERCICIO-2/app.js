7c72c3e2faafb7e7d8c5223fc4ccbfaa19a0a4d3
Magdalena Marchesotti <magdalenamarchesotti@gmail.com>
2022-01-25 11:26:25 -0300

    subida repositorio github

--- EJERCICIO-2/app.js
+++ EJERCICIO-2/app.js
@@ -1,21 +1,37 @@
 // EJERCICIO 2
 let arrayNumeros = [];
-for (let i = 0; i < 4; i ++) {
-arrayNumeros.push (parseInt(prompt('Ingresá un número')));
+let agregarNumeros = '';
+
+for (let i = 0 ; i < 4 ; i++) {
+    agregarNumeros = parseInt(prompt("Ingresá un parámetro"));
+    if (isNaN(agregarNumeros)) {
+        alert ('Sólo números por favor!');
+    } else {
+        arrayNumeros.push(agregarNumeros);
+    }
 }
 
 console.log(arrayNumeros);
 
-const capicua = (numero) => {
-let array = numero.split('');
-console.log(array);
+const capicua = ((numero) => {
+    let array = numero.split('');
+    console.log(array);
 
-let arrayReverse = array.reverse();
-console.log(arrayReverse);
+    let arrayReverse = array.reverse();
+    console.log(arrayReverse);
+    console.log(numero);
+    console.log(arrayReverse.join(''));
 
-if (numero == arrayReverse.join('')) {
-    console.log ("Es un número capicúa")
-} else {
-    console.log ("No es un número capicúa")
+    if (numero == arrayReverse.join('')) {
+        console.log('Es un número capicua');
+    } else {
+        console.log('No es un número capicua');
+    }
+})
+
+for (let i = 0; i < arrayNumeros.length; i++) {
+    capicua(arrayNumeros[i].toString());
 }
-}
+
+arrayNumeros.sort((a, b) => a - b);
+console.log('Array ordenado de menor a mayor:', arrayNumeros)
